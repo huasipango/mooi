7ace22cd7124ad037aab2821a04f92e8082fe9a7
Anthony Cabrera <anthony.cabrera@alluriam.com>
2021-04-28 23:19:38 -0500

    Editado el mapa

--- js/google-map.js
+++ js/google-map.js
@@ -5,13 +5,13 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+    var myLatlng = new google.maps.LatLng(-0.2485146547756644, -79.15044716302337);
     // 39.399872
     // -8.224454
     
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 7,
+        zoom: 18,
 
         // The latitude and longitude to center the map (always required)
         center: myLatlng,
@@ -40,23 +40,21 @@ function init() {
     // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
 
+    var myMarkerOptions = {
+        position: myLatlng,
+        map: mapElement
+    };
+
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
     
-    var addresses = ['New York'];
-
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({
-                position: latlng,
-                map: map,
-                icon: 'images/loc.png'
-            });
-
-        });
-    }
+    var mooiPosition = {lat: -0.2485146547756644,lng: -79.15044716302337};
+	var marker = new google.maps.Marker({
+        position: mooiPosition,
+        map: map,
+        title: 'Möoi Cafetería'
+    });
     
 }
+
 google.maps.event.addDomListener(window, 'load', init);
